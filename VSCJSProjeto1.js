6418bd0384288ba17cfdde79f00a8b54ae1c4576
#14042022 <104284240+14042022@users.noreply.github.com>
2022-07-24 23:56:07 -0300

    Update 2.2.16

--- VSCJSProjeto1.js
+++ VSCJSProjeto1.js
@@ -8,12 +8,13 @@ pagination: {
   el: ".swiper-pagination",
   clickable: true,
   dynamicBullets: true,
+//Buttons
 },
 navigation: {
   nextEl: ".swiper-button-next",
   prevEl: ".swiper-button-prev",
 },
-//breakpoints
+//Breakpoints
 breakpoints:{
     0: {
         slidesPerView: 1,
